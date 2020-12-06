7f928acecd95b7bacf6527adba0387f82ceb0eb7
Konstantin Kormesser <kormesser.k@hotmail.com>
2020-12-06 22:07:47 +0100

    CodingDOJO4 - added client

---
+++ Client/Client/Client/Communication/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Communication
+{
+    class Client
+    {
+        byte[] buffer = new byte[512];
+        Socket _clientSocket;
+        Action<string> _messageInformer;
+        Action _abortInformer;
+        public Client(string ip, int port, Action<string> messageInformer, Action abortInformer)
+        {
+            _abortInformer = abortInformer;
+            _messageInformer = messageInformer;
+            var client = new TcpClient();
+            client.Connect(IPAddress.Parse(ip), port);
+            _clientSocket = client.Client;
+            StartReceiving();
+        }
+
+        public void StartReceiving()
+        {
+            Task.Factory.StartNew(Receive);
+        }
+
+        public void Receive()
+        {
+            string message = "";
+            while(message != "@quit")){
+                int length = _clientSocket.Receive(buffer);
+                message = Encoding.UTF8.GetString(buffer, 0, length);
+                _messageInformer(message);
+            }
+        }
+
+        public void Send(string message)
+        {
+            if (_clientSocket != null)
+                _clientSocket.Send(Encoding.UTF8.GetBytes(message));
+        }
+
+        public void close()
+        {
+            _clientSocket.Close();
+            _abortInformer();
+        }
+    }
+}
---
+++ Client/Client/Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private Client clientCom;
+        private bool isConnected = false;
+        #region PROPERTIES
+        public string ChatName { get; set; }
+        public string Message { get; set; }
+        public ObservableCollection<string> ReceivedMessages { get; set; }
+        public RelayCommand ConnectButtionClickCmd { get; set; }
+        public RelayCommand SendButtonClickCmd { get; set; }
+        #endregion
+
+        public MainViewModel()
+        {
+            Message = "";
+            ReceivedMessages = new ObservableCollection<string>();
+
+            ConnectButtionClickCmd = new RelayCommand(() =>
+            {
+                isConnected = true;
+                clientCom = new Client("127.0.0.1", 10100, new Action<string>(NewMessageReceived), ClientDissconnected);
+            },
+            () =>
+            {
+                return (!isConnected);
+            });
+        }
+
+        private void ClientDissconnected()
+        {
+            isConnected = false;
+            //Updates the button visiblity fires the event or whatever
+            CommandManager.InvalidateRequerySuggestion();
+        }
+
+        private void NewMessageReceived(string message)
+        {
+            App.Current.Dispatcher.Invoke(() => {
+                ReceivedMessages.Add(message);
+            });
+        }
+
+    }
+}
